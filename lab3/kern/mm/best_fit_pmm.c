8ca5ac60a2fc333e8cb9fbfa6d6b0f5b5e8b7219
LoveFridayy <1595662110@qq.com>
2025-11-24 18:56:29 +0800

    delete the old lab5 code

--- lab3/kern/mm/best_fit_pmm.c
+++ lab3/kern/mm/best_fit_pmm.c
@@ -10,7 +10,7 @@
    usually split, and the remainder added to the list as another free block.
    Please see Page 196~198, Section 8.2 of Yan Wei Min's chinese book "Data Structure -- C programming language"
 */
-// LAB2 EXERCISE 1: YOUR CODE
+
 // you should rewrite functions: default_init,default_init_memmap,default_alloc_pages, default_free_pages.
 /*
  * Details of FFMA
@@ -55,7 +55,7 @@
  *               (5.2) reset the fields of pages, such as p->ref, p->flags (PageProperty)
  *               (5.3) try to merge low addr or high addr blocks. Notice: should change some pages's p->property correctly.
  */
-static free_area_t free_area;
+extern free_area_t free_area;
 
 #define free_list (free_area.free_list)
 #define nr_free (free_area.nr_free)
@@ -72,8 +72,13 @@ best_fit_init_memmap(struct Page *base, size_t n) {
     struct Page *p = base;
     for (; p != base + n; p ++) {
         assert(PageReserved(p));
-        /*LAB2 请补充你在lab2中的代码 */ 
-        
+
+        /*LAB2 EXERCISE 2: 2311867*/ 
+        // 清空当前页框的标志和属性信息，并将页框的引用计数设置为0
+        p->flags = 0;           // 清空所有标志
+        set_page_ref(p, 0);     // 引用计数清零
+        ClearPageProperty(p);   // 清除页面属性
+
     }
     base->property = n;
     SetPageProperty(base);
@@ -84,8 +89,18 @@ best_fit_init_memmap(struct Page *base, size_t n) {
         list_entry_t* le = &free_list;
         while ((le = list_next(le)) != &free_list) {
             struct Page* page = le2page(le, page_link);
-            /*LAB2 请补充你在lab2中的代码 */ 
-
+             /*LAB2 EXERCISE 2: 2311867*/ 
+            // 编写代码
+            // 1、当base < page时，找到第一个大于base的页，将base插入到它前面，并退出循环
+            // 2、当list_next(le) == &free_list时，若已经到达链表结尾，将base插入到链表尾部
+            if (base < page) {
+                // 找到第一个地址大于 base 的页
+                list_add_before(le, &(base->page_link));
+                break;
+            }else if (list_next(le) == &free_list) {
+                // 到达链表尾部
+                list_add(le, &(base->page_link));
+            }
         }
     }
 }
@@ -98,14 +113,16 @@ best_fit_alloc_pages(size_t n) {
     }
     struct Page *page = NULL;
     list_entry_t *le = &free_list;
-    size_t min_size = nr_free + 1;
-    /*LAB2 请补充你在lab2中的代码*/ 
-
+    size_t min_size = nr_free + 1; // 初始化为一个极大值
+     /*LAB2 EXERCISE 2: 2311867*/ 
+    // 下面的代码是first-fit的部分代码，请修改下面的代码改为best-fit
+    // 遍历空闲链表，查找满足需求的空闲页框
+    // 如果找到满足需求的页面，记录该页面以及当前找到的最小连续空闲页框数量
     while ((le = list_next(le)) != &free_list) {
         struct Page *p = le2page(le, page_link);
-        if (p->property >= n) {
+        if (p->property >= n && p->property < min_size) {
             page = p;
-            break;
+            min_size = p->property;   // 记录最小满足块
         }
     }
 
@@ -133,29 +150,47 @@ best_fit_free_pages(struct Page *base, size_t n) {
         p->flags = 0;
         set_page_ref(p, 0);
     }
-    /*LAB2 请补充你在lab2中的代码*/
+    /*LAB2 EXERCISE 2: 2311867*/ 
+    // 编写代码
+    // 具体来说就是设置当前页块的属性为释放的页块数、并将当前页块标记为已分配状态、最后增加nr_free的值
+
+    base->property = n;       // 当前块大小
+    SetPageProperty(base);    // 标记为首页
+    nr_free += n;             // 更新总空闲页数
 
     if (list_empty(&free_list)) {
-        list_add(&free_list, &(base->page_link));
-    } else {
-        list_entry_t* le = &free_list;
-        while ((le = list_next(le)) != &free_list) {
-            struct Page* page = le2page(le, page_link);
-            if (base < page) {
-                list_add_before(le, &(base->page_link));
-                break;
-            } else if (list_next(le) == &free_list) {
-                list_add(le, &(base->page_link));
-            }
-        }
-    }
+          list_add(&free_list, &(base->page_link));
+      } else {
+          list_entry_t* le = &free_list;
+          while ((le = list_next(le)) != &free_list) {
+              struct Page* page = le2page(le, page_link);
+              if (base < page) {
+                  list_add_before(le, &(base->page_link));
+                  break;
+              } else if (list_next(le) == &free_list) {
+                  list_add(le, &(base->page_link));
+              }
+          }
+      }
 
+ 
     list_entry_t* le = list_prev(&(base->page_link));
-    if (le != &free_list) {
-        p = le2page(le, page_link);
-        /*LAB2 请补充你在lab2中的代码*/
-        
-    }
+      if (le != &free_list) {
+          p = le2page(le, page_link);
+          /*LAB2 EXERCISE 2: 2311867*/ 
+         // 编写代码
+        // 1、判断前面的空闲页块是否与当前页块是连续的，如果是连续的，则将当前页块合并到前面的空闲页块中
+        // 2、首先更新前一个空闲页块的大小，加上当前页块的大小
+        // 3、清除当前页块的属性标记，表示不再是空闲页块
+        // 4、从链表中删除当前页块
+        // 5、将指针指向前一个空闲页块，以便继续检查合并后的连续空闲页块
+          if (p + p->property == base) {        // 若地址连续
+              p->property += base->property;    // 合并大小
+              ClearPageProperty(base);
+              list_del(&(base->page_link));
+              base = p;                         // 合并之后 base 指向前块
+          }
+      }
 
     le = list_next(&(base->page_link));
     if (le != &free_list) {
@@ -224,7 +259,7 @@ basic_check(void) {
     free_page(p2);
 }
 
-// LAB2: below code is used to check the best fit allocation algorithm (your EXERCISE 1) 
+// LAB2: below code is used to check the best fit allocation algorithm 
 // NOTICE: You SHOULD NOT CHANGE basic_check, default_check functions!
 static void
 best_fit_check(void) {
